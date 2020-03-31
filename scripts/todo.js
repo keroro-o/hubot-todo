c76305e33678d1350c7a542308cfaa86d1f935e6
keroro-o <obata19760205akinobu@outlook.jp>
2020-03-31 23:26:35 +0900

    listコマンドとdonelistコマンドを追加実装

--- scripts/todo.js
+++ scripts/todo.js
@@ -28,4 +28,10 @@ module.exports = (robot) => {
     todo.del(task);
     msg.send('削除しました： ' + task);
   });
+  robot.respond(/list/i, (msg) => {
+    msg.send(todo.list().join('\n'));
+  });
+  robot.respond(/donelist/i, (msg) => {
+    msg.send(todo.donelist().join('\n'));
+  });
 };
